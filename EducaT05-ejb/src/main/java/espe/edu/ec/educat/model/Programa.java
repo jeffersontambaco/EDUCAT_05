20812b8fbe10f8ac31140241626b71d3d89fb812
Alejandro Torres <Alejandro Torres@AlejandroTorres>
2017-07-16 15:57:34 -0500

    Documentar la clase Programa en el paquete model.

--- EducaT05-ejb/src/main/java/espe/edu/ec/educat/model/Programa.java
+++ EducaT05-ejb/src/main/java/espe/edu/ec/educat/model/Programa.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package espe.edu.ec.educat.model;
 
 import java.io.Serializable;
@@ -24,7 +20,7 @@ import javax.validation.constraints.Size;
 
 /**
  *
- * @author jeffe
+ * @author Alejandro Torres
  */
 @Entity
 @Table(name = "programa")
@@ -38,24 +34,44 @@ public class Programa implements Serializable {
     @NotNull
     @Size(min = 1, max = 8)
     @Column(name = "COD_PROGRAMA", nullable = false, length = 8)
+    /*
+    * llave primaria de 8 caracteres que identifica al programa que ofrece el 
+    * instituto
+    */
     private String codPrograma;
     @Basic(optional = false)
     @NotNull
     @Size(min = 1, max = 100)
     @Column(name = "NOMBRE", nullable = false, length = 100)
+    /*
+    * atributo que almacena el nombre del programa que ofrece el instituto
+    */
     private String nombre;
     @Size(max = 4000)
     @Column(name = "DESCRIPCION", length = 4000)
+    /*
+    * atributo que almacena una breve descripcion del programa
+    */
     private String descripcion;
     @Basic(optional = false)
     @NotNull
     @Column(name = "DURACION", nullable = false)
+    /*
+    * atributo que almacena la duracion del programa en horas
+    */
     private short duracion;
     @Column(name = "FECHA_INICIO")
     @Temporal(TemporalType.DATE)
+    /*
+    * Atributo que almacena la informacion de la fecha en la que inicio el 
+    * programa
+    */
     private Date fechaInicio;
     @Column(name = "FECHA_FIN")
     @Temporal(TemporalType.DATE)
+    /*
+    * Atributo que almacena la fecha en la que el programa ha terminado
+    */
     private Date fechaFin;
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "programa")
     private List<ProgramaCurso> programaCursoList;
