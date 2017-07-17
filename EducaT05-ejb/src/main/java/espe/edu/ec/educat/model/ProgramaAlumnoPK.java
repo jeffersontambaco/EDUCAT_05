df7d409f678699a44a4037fc55d9962385c4d0ac
DiegoYandun <DiegoYandun@DESKTOP-RMBFMNR>
2017-07-17 00:27:15 -0500

    Documentacion Clases

--- EducaT05-ejb/src/main/java/espe/edu/ec/educat/model/ProgramaAlumnoPK.java
+++ EducaT05-ejb/src/main/java/espe/edu/ec/educat/model/ProgramaAlumnoPK.java
@@ -1,7 +1,11 @@
 /*
+@(#)ProgramaAlumnoPK.java     1.0 02/07/2017
+*
+*Copyright 2017 Grupo_05 All rights reserved.
+*Grupo_05 Propietary. Use is subject to license terms
  * The MIT License
  *
- * Copyright 2017 Jonathan.
+ * Copyright 2017 Grupo_05.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -20,6 +24,9 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
+ **/
+/**
+ * Entidad que almacena las llaves primarias de ProgramaAlumno
  */
 
 
@@ -33,7 +40,7 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 /**
- *
+ * @version 1.0
  * @author diegoyandun
  */
 @Embeddable
